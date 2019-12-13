35261dfd32b2eb4261afca85aab13b491791166d
narcotic1998 <csknaren@gmail.com>
2019-12-14 04:50:47 +0530

    I'm on my way

--- app.js
+++ app.js
@@ -16,4 +16,6 @@ app.get("/api/name?:startwith", (req, res) => {
     console.log("asf")
 })
 
-//app.listen(5000, () => {console.log("listening on port 5000.....")})
+let port = process.env.PORT || 5000
+
+app.listen(port, () => {console.log(`listening on port ${port}.....`)})
