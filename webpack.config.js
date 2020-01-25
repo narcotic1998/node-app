3672f6d70298b9dc3275816a2bd3f0895a5228b0
narcotic1998 <csknaren@gmail.com>
2020-01-26 01:10:25 +0530

    webpack changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: './js/index.js',
+    entry: './dist/main.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
